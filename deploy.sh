8ba61efee475972c17f69e248701296964e92655
shashi1965 <127210074+shashi1965@users.noreply.github.com>
2023-11-10 15:42:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-if [ "$GIT_BRANCH" == "origin/dev" ]; then
-  docker login -u jkapse51 -p dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
+if [ "$1" == "dev" ]; then
+    docker login -u jkapse51 -p dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
+    ./build.sh
+    docker tag reactimg:v1 dev/docker-repo:latest
+    docker push dev/docker-repo:latest
+    echo "Image successfully pushed to the 'dev' repository."
 
-  ./build.sh
-
-  docker tag your-image dev/docker-repo:latest
-
-  docker push dev/docker-repo:latest
-
-  echo "Image successfully pushed to the 'dev' repository."
-  
-elif [ "$GIT_BRANCH" == "origin/main" ]; then
-  docker login -u jkapse51 -p  dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
-
-  
-  ./build.sh
-
-  
-  docker tag your-image prod/dockerhub:latest
-
-  docker push prod/dockerhub:latest
-
-  echo "Image successfully pushed to the 'prod' repository."
+elif [ "$1" == "prod" ]; then
+    docker login -u jkapse51 -p dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
+    ./build.sh
+    docker tag reactimg:v1 prod/dockerhub:latest
+    docker push prod/dockerhub:latest
+    echo "Image successfully pushed to the 'prod' repository."
 
 else
-  echo "Deployment error/fail: Invalid branch."
-  exit 1
+    echo "Invalid argument. Use 'dev' or 'prod'."
 fi
-
