14c757c5a13d4fabc2fa0f1ae1d869c9d5227d42
shashi1965 <127210074+shashi1965@users.noreply.github.com>
2023-11-17 15:49:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 if [ "$1" == "dev" ]; then
     docker login -u jkapse51 -p dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
     ./build.sh
-    docker tag reactimg:v1 docker.io/jkapse51/dev/docker-repo:latest
-    docker push docker.io/jkapse51/dev/docker-repo:latest
+    docker tag reactimg:v1 jkapse51/dev:docker-repo
+    docker push jkapse51/dev:docker-repo
     echo "Image successfully pushed to the 'dev' repository."
 
 elif [ "$1" == "prod" ]; then
     docker login -u jkapse51 -p dckr_pat_0ZKc5p1mi4ej-R8xrg0B62bP8rg
     ./build.sh
-    docker tag reactimg:v1 docker.io/jkapse51/prod/dockerhub:latest
-    docker push docker.io/jkapse51/prod/dockerhub:latest
+    docker tag reactimg:v1 jkapse51/dev:docker-repo
+    docker push jkapse51/dev:docker-repo
     echo "Image successfully pushed to the 'prod' repository."
 
 else
